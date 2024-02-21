502f7896af257d371c544b64c5630583beccc64b
Maruf Siddiki Galib <145563201+MarufSiddikiGalib@users.noreply.github.com>
2024-02-22 00:36:53 +0600

    Updated css

--- src/Components/OpenAi.js
+++ src/Components/OpenAi.js
@@ -2,6 +2,7 @@ const {Configuration, OpenAiApi} = require('openai');
 const configuration = new Configuration({apiKey: "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"});
 const openai = new OpenAiApi(configuration);
 
+
 export async function sendToOpenAI(message){
   
     const res = await openai.createCompletion({
@@ -15,6 +16,6 @@ export async function sendToOpenAI(message){
 
 
     });
-
+ 
     return res.data.choices[0].text;
 }
